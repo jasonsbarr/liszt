d9b5312b364e76aa0cd059f25376242f7a28110f
Jason Barr <jason.barr@metronetinc.com>
2022-10-28 08:31:05 -0500

    Lex integers

--- src/diagnostics/DiagnosticBag.ts
+++ src/diagnostics/DiagnosticBag.ts
@@ -33,8 +33,6 @@ export class DiagnosticBag {
 
   private append(diagnostic: Diagnostic) {
     this.diagnostics.push(diagnostic);
-
-    return this;
   }
 
   *[Symbol.iterator]() {
--- src/lexer/Lexer.ts
+++ src/lexer/Lexer.ts
@@ -1,35 +1,32 @@
 import os from "os";
+import { DiagnosticBag } from "../diagnostics/DiagnosticBag";
+import { isDigit } from "./helpers";
 
 export class Token {
   constructor(
     public type: TokenTypes,
     public name: TokenNames,
     public value: string,
-    public trivia: string = "",
-    public location: SrcLoc
+    public location: SrcLoc,
+    public trivia: string = ""
   ) {}
 
   public static create(
     type: TokenTypes,
     name: TokenNames,
     value: string,
-    trivia: string = "",
-    location: SrcLoc
+    location: SrcLoc,
+    trivia: string = ""
   ) {
-    return new Token(type, name, value, trivia, location);
+    return new Token(type, name, value, location, trivia);
   }
 }
 
 export class SrcLoc {
-  constructor(
-    public pos: number,
-    public line: number,
-    public col: number,
-    public file: string
-  ) {}
+  constructor(public pos: number, public line: number, public col: number) {}
 
-  public static create(pos: number, line: number, col: number, file: string) {
-    return new SrcLoc(pos, line, col, file);
+  public static create(pos: number, line: number, col: number) {
+    return new SrcLoc(pos, line, col);
   }
 }
 
@@ -104,20 +101,98 @@ export enum TokenNames {
   Mod,
 }
 
+export class LexResult {
+  constructor(
+    public tokens: TokenBag,
+    public diagnostics: DiagnosticBag,
+    public file: string
+  ) {}
+
+  public static create(
+    tokens: TokenBag,
+    diagnostics: DiagnosticBag,
+    file: string
+  ) {
+    return new LexResult(tokens, diagnostics, file);
+  }
+}
+
+export class TokenBag {
+  public tokens: Token[];
+
+  constructor() {
+    this.tokens = [];
+  }
+
+  public static create() {
+    return new TokenBag();
+  }
+
+  private append(token: Token) {
+    this.tokens.push(token);
+  }
+
+  public addIntegerToken(
+    value: string,
+    pos: number,
+    line: number,
+    col: number,
+    trivia: string
+  ) {
+    const token = Token.create(
+      TokenTypes.Integer,
+      TokenNames.Integer,
+      value,
+      SrcLoc.create(pos, line, col),
+      trivia
+    );
+
+    this.append(token);
+  }
+}
+
 export class Lexer {
   public fileName: string;
   public input: Input;
+  public tokens: TokenBag;
+  public diagnostics: DiagnosticBag;
 
   constructor(input: string, fileName: string) {
     this.fileName = fileName;
     this.input = Input.create(input);
+    this.tokens = TokenBag.create();
+    this.diagnostics = DiagnosticBag.create();
   }
 
   public static create(input: string, fileName: string = "<stdin>") {
     return new Lexer(input, fileName);
   }
 
+  public readNumber(trivia = "") {
+    let digitString = this.input.readWhile(isDigit);
+
+    this.tokens.addIntegerToken(
+      digitString,
+      this.input.pos,
+      this.input.line,
+      this.input.col,
+      trivia
+    );
+  }
+
   public tokenize() {
-    while (!this.input.eof()) {}
+    while (!this.input.eof()) {
+      let char = this.input.peek();
+
+      if (isDigit(char)) {
+        this.readNumber();
+      } else {
+        throw new Error(
+          `Unrecognized character ${char} (${this.input.col}:${this.input.line})`
+        );
+      }
+    }
+
+    return LexResult.create(this.tokens, this.diagnostics, this.fileName);
   }
 }
