769030da399b297b55a7b22a39446671606f826b
Jason Barr <jason.barr@metronetinc.com>
2022-10-28 20:20:39 -0500

    Check type for integer literals

---
+++ src/typechecker/check.ts
@@ -0,0 +1,12 @@
+import { ASTNode } from "../parser/ast/ASTNode";
+import { isSubtype } from "./isSubtype";
+import { synth } from "./synth";
+import { Type } from "./Type";
+
+export const check = (ast: ASTNode, t: Type) => {
+  const synthType = synth(ast);
+
+  if (isSubtype(synthType, t)) return true;
+
+  throw new Error(`Expected ${t.name}, got ${synthType.name}`);
+};
--- src/typechecker/synth.ts
+++ src/typechecker/synth.ts
@@ -1,12 +1,13 @@
 import { Type } from "./Type";
 import { SyntaxNodes } from "../parser/ast/SyntaxNodes";
 import { ASTNode } from "../parser/ast/ASTNode";
-import { IntegerLiteral } from "../parser/ast/IntegerLiteral";
 
 export const synth = (ast: ASTNode) => {
   switch (ast.kind) {
     case SyntaxNodes.IntegerLiteral:
       return synthInteger();
+    default:
+      throw new Error(`Unknown type for expression type ${ast.kind}`);
   }
 };
 
