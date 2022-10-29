3399b7e0b7f8ae2e5f020ce08aac44f9ebfd27de
Jason Barr <jason.barr@metronetinc.com>
2022-10-28 20:39:28 -0500

    Create bound tree class

---
+++ src/typechecker/tree/BoundTree.ts
@@ -0,0 +1,27 @@
+import { DiagnosticBag } from "../../diagnostics/DiagnosticBag";
+import { TokenBag } from "../../lexer/TokenBag";
+import { SyntaxTree } from "../../parser/ast/SyntaxTree";
+import { BoundProgramNode } from "./BoundProgramNode";
+
+export class BoundTree extends SyntaxTree {
+  constructor(
+    root: BoundProgramNode,
+    tokens: TokenBag,
+    diagnostics: DiagnosticBag,
+    source: string,
+    file: string
+  ) {
+    super(root, tokens, diagnostics, source, file);
+    this.diagnostics = DiagnosticBag.from(this.diagnostics);
+  }
+
+  public static new(
+    root: BoundProgramNode,
+    tokens: TokenBag,
+    diagnostics: DiagnosticBag,
+    source: string,
+    file: string
+  ) {
+    return new BoundTree(root, tokens, diagnostics, source, file);
+  }
+}
