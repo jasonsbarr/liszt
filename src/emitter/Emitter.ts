707fa9be01acefb7a1a0fc0bbe9cca324b0fde2d
Jason Barr <jason.barr@metronetinc.com>
2022-10-28 20:41:11 -0500

    Use bound tree in code emitter

--- src/emitter/Emitter.ts
+++ src/emitter/Emitter.ts
@@ -1,13 +1,13 @@
 import { ASTNode } from "../parser/ast/ASTNode";
-import { IntegerLiteral } from "../parser/ast/IntegerLiteral";
-import { ProgramNode } from "../parser/ast/ProgramNode";
-import { SyntaxNodes } from "../parser/ast/SyntaxNodes";
-import { SyntaxTree } from "../parser/ast/SyntaxTree";
+import { BoundIntegerLiteral } from "../typechecker/tree/BoundIntegerLiteral";
+import { BoundNodes } from "../typechecker/tree/BoundNodes";
+import { BoundProgramNode } from "../typechecker/tree/BoundProgramNode";
+import { BoundTree } from "../typechecker/tree/BoundTree";
 
 export class Emitter {
   constructor(public tree: SyntaxTree) {}
 
-  public static new(tree: SyntaxTree) {
+  public static new(tree: BoundTree) {
     return new Emitter(tree);
   }
 
@@ -19,16 +19,16 @@ export class Emitter {
 
   private emitNode(node: ASTNode) {
     switch (node.kind) {
-      case SyntaxNodes.ProgramNode:
-        return this.emitProgram(node as ProgramNode);
-      case SyntaxNodes.IntegerLiteral:
-        return this.emitInteger(node as IntegerLiteral);
+      case BoundNodes.BoundProgramNode:
+        return this.emitProgram(node as BoundProgramNode);
+      case BoundNodes.BoundIntegerLiteral:
+        return this.emitInteger(node as BoundIntegerLiteral);
       default:
         throw new Error(`Unknown syntax node type ${node.kind}`);
     }
   }
 
-  private emitProgram(node: ProgramNode) {
+  private emitProgram(node: BoundProgramNode) {
     const nodes = node.children;
     let code = "";
 
@@ -39,7 +39,7 @@ export class Emitter {
     return code;
   }
 
-  private emitInteger(node: IntegerLiteral) {
+  private emitInteger(node: BoundIntegerLiteral) {
     return `${node.token.value}n`;
   }
 }
