9ab70da84db6792161b1f37cd0c1f356d01be414
Jason Barr <jason.barr@metronetinc.com>
2022-11-02 17:20:15 -0500

    Chalk sucks for this

--- src/cli/repl.ts
+++ src/cli/repl.ts
@@ -1,6 +1,5 @@
 import vm from "vm";
 import readlineSync from "readline-sync";
-import chalk from "chalk";
 import { compile } from "./compile";
 
 const READ = () => readlineSync.question("liszt> ");
@@ -11,6 +10,6 @@ while (true) {
   try {
     PRINT(EVAL(READ()));
   } catch (e: any) {
-    console.log(chalk.redBright(e.message));
+    console.log(e.message);
   }
 }
