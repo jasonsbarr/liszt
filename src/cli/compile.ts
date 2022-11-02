35296957a398dafd4bc00ae5fab645ee8171481d
Jason Barr <jason.barr@metronetinc.com>
2022-11-02 16:59:46 -0500

    Add default filename

--- src/cli/compile.ts
+++ src/cli/compile.ts
@@ -3,5 +3,5 @@ import { tokenize } from "../lexer/tokenize";
 import { parse } from "../parser/parse";
 import { checktypes } from "../typechecker/checktypes";
 
-export const compile = (input: string, file: string) =>
+export const compile = (input: string, file: string = "<stdin>") =>
   emit(checktypes(parse(tokenize(input, file))));
