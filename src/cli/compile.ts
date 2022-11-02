5cb284da7d73681f0fdb12cad45ca2aa4bfdc5e7
Jason Barr <jason.barr@metronetinc.com>
2022-11-02 16:59:02 -0500

    Create compile function

---
+++ src/cli/compile.ts
@@ -0,0 +1,8 @@
+import { emit } from "../emitter/emit";
+import { tokenize } from "../lexer/tokenize";
+import { parse } from "../parser/parse";
+import { checktypes } from "../typechecker/checktypes";
+
+export const compile = (input: string, file: string) => {
+  return emit(checktypes(parse(tokenize(input, file))));
+};
