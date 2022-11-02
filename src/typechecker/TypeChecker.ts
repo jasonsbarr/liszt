f062a9625f0915b9ab43b8fd8bf15b9cc22f6f26
Jason Barr <jason.barr@metronetinc.com>
2022-11-02 10:25:57 -0500

    Stub out checkProgram and checkInteger nodes

--- src/typechecker/TypeChecker.ts
+++ src/typechecker/TypeChecker.ts
@@ -1,6 +1,8 @@
 import { SyntaxTree } from "../parser/ast/SyntaxTree";
 import { SyntaxNodes } from "../parser/ast/SyntaxNodes";
 import { ASTNode } from "../parser/ast/ASTNode";
+import { ProgramNode } from "../parser/ast/ProgramNode";
+import { IntegerLiteral } from "../parser/ast/IntegerLiteral";
 
 export class TypeChecker {
   constructor(public tree: SyntaxTree) {}
@@ -15,5 +17,18 @@ export class TypeChecker {
     return this.checkNode(program);
   }
 
-  private checkNode(node: ASTNode) {}
+  private checkNode(node: ASTNode) {
+    switch (node.kind) {
+      case SyntaxNodes.ProgramNode:
+        return this.checkProgram(node as ProgramNode);
+      case SyntaxNodes.IntegerLiteral:
+        return this.checkIntegerLiteral(node as IntegerLiteral);
+      default:
+        throw new Error(`Unknown AST node type ${node.kind}`);
+    }
+  }
+
+  private checkProgram(node: ProgramNode) {}
+
+  private checkIntegerLiteral(node: IntegerLiteral) {}
 }
