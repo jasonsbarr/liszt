9b8b2cdadf5748073c2b4b9c2b9ee04a17e93338
Jason Barr <jason.barr@metronetinc.com>
2022-11-03 12:53:35 -0500

    Add rest of primitive type constructors

--- src/typechecker/constructors.ts
+++ src/typechecker/constructors.ts
@@ -1,3 +1,17 @@
-import { IntegerType } from "./Types";
+import {
+  BooleanType,
+  FloatType,
+  IntegerType,
+  NilType,
+  StringType,
+} from "./Types";
 
 export const integer = IntegerType.new();
+
+export const float = FloatType.new();
+
+export const string = StringType.new();
+
+export const boolean = BooleanType.new();
+
+export const nil = NilType.new();
