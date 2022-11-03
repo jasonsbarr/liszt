74ff7c3be1d19a492805d11f90c82a6d1d41e614
Jason Barr <jason.barr@metronetinc.com>
2022-11-03 12:52:10 -0500

    Add types for float, string, boolean, and nil

--- src/typechecker/Types.ts
+++ src/typechecker/Types.ts
@@ -12,4 +12,44 @@ export class IntegerType extends BaseType {
   }
 }
 
-export type Type = IntegerType;
+export class FloatType extends BaseType {
+  constructor() {
+    super("Float");
+  }
+
+  public static new() {
+    return new FloatType();
+  }
+}
+
+export class StringType extends BaseType {
+  constructor() {
+    super("String");
+  }
+
+  public static new() {
+    return new StringType();
+  }
+}
+
+export class BooleanType extends BaseType {
+  constructor() {
+    super("Boolean");
+  }
+
+  public static new() {
+    return new BooleanType();
+  }
+}
+
+export class NilType extends BaseType {
+  constructor() {
+    super("Nil");
+  }
+
+  public static new() {
+    return new NilType();
+  }
+}
+
+export type Type = IntegerType | FloatType | StringType | BooleanType | NilType;
