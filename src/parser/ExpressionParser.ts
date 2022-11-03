7397d50ef01859073152192dea618d919c2353f3
Jason Barr <jason.barr@metronetinc.com>
2022-11-03 12:40:58 -0500

    Parse string, boolean, and nil literals into AST nodes

--- src/parser/ExpressionParser.ts
+++ src/parser/ExpressionParser.ts
@@ -1,11 +1,17 @@
 import { LexResult } from "../lexer/LexResult";
-import { TokenNames } from "../lexer/TokenNames";
+import { TokenTypes } from "../lexer/TokenTypes";
 import { ASTNode } from "./ast/ASTNode";
+import { BooleanLiteral } from "./ast/BooleanLiteral";
 import { IntegerLiteral } from "./ast/IntegerLiteral";
+import { NilLiteral } from "./ast/NilLiteral";
+import { StringLiteral } from "./ast/StringLiteral";
 import { LHVParser } from "./LHVParser";
 
 const nudAttributes = {
   Integer: { prec: 0, assoc: "none" },
+  String: { prec: 0, assoc: "none" },
+  Boolean: { prec: 0, assoc: "none" },
+  Nil: { prec: 0, assoc: "none" },
 };
 
 const ledAttributes = {};
@@ -17,9 +23,15 @@ export abstract class ExpressionParser extends LHVParser {
 
   private parseAtom(): ASTNode {
     const token = this.reader.next();
-    switch (token.name) {
-      case TokenNames.Integer:
+    switch (token.type) {
+      case TokenTypes.Integer:
         return IntegerLiteral.new(token, token.location);
+      case TokenTypes.String:
+        return StringLiteral.new(token, token.location);
+      case TokenTypes.Boolean:
+        return BooleanLiteral.new(token, token.location);
+      case TokenTypes.Nil:
+        return NilLiteral.new(token, token.location);
       default:
         throw new Error(`Unknown token kind ${token.name}`);
     }
