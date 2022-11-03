448175c64ed838c19f0529dc65538ef3a0abb51a
Jason Barr <jason.barr@metronetinc.com>
2022-11-02 19:46:21 -0500

    Add helpers for integer values

--- src/lexer/helpers.ts
+++ src/lexer/helpers.ts
@@ -5,3 +5,13 @@ export const isWhitespace = (c: string) => / \t/.test(c);
 export const isNewline = (c: string) => /\n|\r/.test(c);
 
 export const isComment = (c: string) => /;/.test(c);
+
+export const isAlphaNumeric = (c: string) => /[0-9a-zA-Z]/.test(c);
+
+export const isDecimalDigit = (str: string) => /[0-9_]+/.test(str);
+
+export const isHexDigit = (str: string) => /0x[0-9a-fA-F]+/.test(str);
+
+export const isOctDigit = (str: string) => /0o[0-7]+/.test(str);
+
+export const isBinDigit = (str: string) => /0b[0-1]+/.test(str);
