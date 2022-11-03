cebef3b90f780810e660624c5683dd5b1d3b82bc
Jason Barr <jason.barr@metronetinc.com>
2022-11-03 12:55:51 -0500

    Add validators for primitive types

--- src/typechecker/validators.ts
+++ src/typechecker/validators.ts
@@ -1,3 +1,18 @@
-import { Type, IntegerType } from "./Types";
+import {
+  Type,
+  IntegerType,
+  FloatType,
+  StringType,
+  BooleanType,
+  NilType,
+} from "./Types";
 
 export const isInteger = (t: Type): t is IntegerType => t.name === "Integer";
+
+export const isFloat = (t: Type): t is FloatType => t.name === "Float";
+
+export const isString = (t: Type): t is StringType => t.name === "String";
+
+export const isBoolean = (t: Type): t is BooleanType => t.name === "Boolean";
+
+export const isNil = (t: Type): t is NilType => t.name === "Nil";
