b9cd185175ce534c9cb84cc179f74852cd0062e3
Jason Barr <jason.barr@metronetinc.com>
2022-11-03 13:05:49 -0500

    Export types, constructors, and validators for primitive types

--- src/typechecker/Type.ts
+++ src/typechecker/Type.ts
@@ -4,15 +4,29 @@ import * as Validators from "./validators";
 
 module Type {
   // types
+  export type Number = Types.NumberType;
   export type Integer = Types.IntegerType;
+  export type Float = Types.FloatType;
+  export type String = Types.StringType;
+  export type Boolean = Types.BooleanType;
+  export type Nil = Types.NilType;
 
   export type Type = Types.Type;
 
   // constructors
   export const integer = Constructors.integer;
+  export const float = Constructors.float;
+  export const string = Constructors.string;
+  export const boolean = Constructors.boolean;
+  export const nil = Constructors.nil;
 
   // validators
+  export const isNumber = Validators.isNumber;
   export const isInteger = Validators.isInteger;
+  export const isFloat = Validators.isFloat;
+  export const isString = Validators.isString;
+  export const isBoolean = Validators.isBoolean;
+  export const isNil = Validators.isNil;
 }
 
 type Type = Type.Type;
