e4392eb4635b19ec424227c2d3ff86d70d095ca1
Jason Barr <jason.barr@metronetinc.com>
2022-11-03 13:25:14 -0500

    Emit code for primitive types

--- src/emitter/Emitter.ts
+++ src/emitter/Emitter.ts
@@ -1,7 +1,11 @@
 import { ASTNode } from "../parser/ast/ASTNode";
+import { BoundBooleanLiteral } from "../typechecker/tree/BoundBooleanLiteral";
+import { BoundFloatLiteral } from "../typechecker/tree/BoundFloatLiteral";
 import { BoundIntegerLiteral } from "../typechecker/tree/BoundIntegerLiteral";
+import { BoundNilLiteral } from "../typechecker/tree/BoundNilLiteral";
 import { BoundNodes } from "../typechecker/tree/BoundNodes";
 import { BoundProgramNode } from "../typechecker/tree/BoundProgramNode";
+import { BoundStringLiteral } from "../typechecker/tree/BoundStringLiteral";
 import { BoundTree } from "../typechecker/tree/BoundTree";
 
 export class Emitter {
@@ -23,6 +27,14 @@ export class Emitter {
         return this.emitProgram(node as BoundProgramNode);
       case BoundNodes.BoundIntegerLiteral:
         return this.emitInteger(node as BoundIntegerLiteral);
+      case BoundNodes.BoundFloatLiteral:
+        return this.emitFloat(node as BoundFloatLiteral);
+      case BoundNodes.BoundStringLiteral:
+        return this.emitString(node as BoundStringLiteral);
+      case BoundNodes.BoundBooleanLiteral:
+        return this.emitBoolean(node as BoundBooleanLiteral);
+      case BoundNodes.BoundNilLiteral:
+        return this.emitNil(node as BoundNilLiteral);
       default:
         throw new Error(`Unknown syntax node type ${node.kind}`);
     }
@@ -41,5 +53,21 @@ export class Emitter {
 
   private emitInteger(node: BoundIntegerLiteral) {
     return `${node.token.value}n`;
+  }
+
+  private emitFloat(node: BoundFloatLiteral) {
+    return node.token.value;
+  }
+
+  private emitString(node: BoundStringLiteral) {
+    return node.token.value;
+  }
+
+  private emitBoolean(node: BoundBooleanLiteral) {
+    return node.token.value;
+  }
+
+  private emitNil(node: BoundNilLiteral) {
+    return "null";
   }
 }
