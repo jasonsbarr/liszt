c830551c78be8dc9ac03950e63e4e4a1a2e95617
Jason Barr <jason.barr@metronetinc.com>
2022-11-04 09:23:52 -0500

    Braces, not brackets

--- src/syntax/lexer/helpers.ts
+++ src/syntax/lexer/helpers.ts
@@ -61,8 +61,8 @@ export type ops = keyof typeof OPERATORS;
 
 export const PUNC = {
   ":": TokenNames.Colon,
-  "{": TokenNames.LBracket,
-  "}": TokenNames.RBracket,
+  "{": TokenNames.LBrace,
+  "}": TokenNames.RBrace,
   ".": TokenNames.Dot,
 };
 
