ad05badb14e478bb135a34be0a89feb67d8d6eae
Jason Barr <jason.barr@metronetinc.com>
2022-11-07 09:26:32 -0600

    Move EOF token to bottom of list

--- src/syntax/lexer/TokenTypes.ts
+++ src/syntax/lexer/TokenTypes.ts
@@ -1,11 +1,11 @@
 export enum TokenTypes {
   Integer = "IntegerToken",
   Float = "FloatToken",
-  EOF = "EOFToken",
   String = "StringToken",
   Boolean = "BooleanToken",
   Nil = "NilToken",
   Punctuation = "PunctuationToken",
   Operator = "OperatorToken",
   Keyword = "KeywordToken",
+  EOF = "EOFToken",
 }
