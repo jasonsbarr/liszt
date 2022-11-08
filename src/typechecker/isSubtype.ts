8ce73feea599feea053e5a25e0ea22aeabeac244
Jason Barr <jason.barr@metronetinc.com>
2022-11-08 10:27:18 -0600

    Add object type to isSubtype

--- src/typechecker/isSubtype.ts
+++ src/typechecker/isSubtype.ts
@@ -1,3 +1,4 @@
+import { propType } from "./propType";
 import { Type } from "./Type";
 
 export const isSubtype = (t1: Type, t2: Type) => {
@@ -5,5 +6,12 @@ export const isSubtype = (t1: Type, t2: Type) => {
   else if (Type.isString(t1) && Type.isString(t2)) return true;
   else if (Type.isBoolean(t1) && Type.isBoolean(t2)) return true;
   else if (Type.isNil(t1) && Type.isNil(t2)) return true;
-  else return false;
+  else if (Type.isObject(t1) && Type.isObject(t2)) {
+    return t2.properties.every(({ name: bName, type: bType }): boolean => {
+      const aType = propType(t1, bName);
+
+      if (!aType) return false;
+      return isSubtype(aType, bType);
+    });
+  } else return false;
 };
