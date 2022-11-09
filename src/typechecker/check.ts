20b7d35577e9f8d4545ac09db373c06b43caa73d
Jason Barr <jason.barr@metronetinc.com>
2022-11-09 12:02:40 -0600

    Check objects

--- src/typechecker/check.ts
+++ src/typechecker/check.ts
@@ -1,12 +1,58 @@
 import { ASTNode } from "../syntax/parser/ast/ASTNode";
+import { Identifier } from "../syntax/parser/ast/Identifier";
+import { ObjectLiteral } from "../syntax/parser/ast/ObjectLiteral";
+import { SyntaxNodes } from "../syntax/parser/ast/SyntaxNodes";
 import { isSubtype } from "./isSubtype";
+import { propType } from "./propType";
 import { synth } from "./synth";
 import { Type } from "./Type";
 
 export const check = (ast: ASTNode, t: Type) => {
+  if (ast.kind === SyntaxNodes.ObjectLiteral && Type.isObject(t)) {
+    return checkObject(ast as ObjectLiteral, t);
+  }
+
   const synthType = synth(ast);
 
   if (isSubtype(synthType, t)) return true;
 
   throw new Error(`Expected ${t.name}, got ${synthType.name}`);
+};
+
+type ObjectProps = {
+  name: string;
+  expr: ASTNode;
+  key: Identifier;
+};
+
+const checkObject = (ast: ObjectLiteral, type: Type.Object) => {
+  const objProps: ObjectProps[] = ast.properties.map((prop) => {
+    if (prop.key.kind !== SyntaxNodes.Identifier) {
+      throw new Error(
+        `Object property names must be valid identifiers; ${prop.key.kind} given`
+      );
+    }
+
+    return { name: prop.key.name, expr: prop.value, key: prop.key };
+  });
+
+  type.properties.forEach(({ name }) => {
+    const prop = objProps.find(({ name: propName }) => propName === name);
+
+    if (!prop) {
+      throw new Error(`Object is missing property ${name}`);
+    }
+  });
+
+  objProps.forEach(({ name, expr }) => {
+    const pType = propType(type, name);
+
+    if (pType) {
+      check(expr, pType);
+    } else {
+      throw new Error(`Property ${name} does not exist on type ${type}`);
+    }
+  });
+
+  return true;
 };
