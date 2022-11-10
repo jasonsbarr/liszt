6e3e28eadee11427d9233f645829d8e824662737
Jason Barr <jason.barr@metronetinc.com>
2022-11-10 13:50:27 -0600

    Check for function subtype

--- src/typechecker/isSubtype.ts
+++ src/typechecker/isSubtype.ts
@@ -1,7 +1,7 @@
 import { propType } from "./propType";
 import { Type } from "./Type";
 
-export const isSubtype = (t1: Type, t2: Type) => {
+export const isSubtype = (t1: Type, t2: Type): boolean => {
   if (Type.isNumber(t1) && Type.isNumber(t2)) return true;
   else if (Type.isString(t1) && Type.isString(t2)) return true;
   else if (Type.isBoolean(t1) && Type.isBoolean(t2)) return true;
@@ -13,5 +13,11 @@ export const isSubtype = (t1: Type, t2: Type) => {
       if (!aType) return false;
       return isSubtype(aType, bType);
     });
+  } else if (Type.isFunction(t1) && Type.isFunction(t2)) {
+    return (
+      t1.args.length === t2.args.length &&
+      t1.args.every((a, i) => isSubtype(t2.args[i], a)) &&
+      isSubtype(t1.ret, t2.ret)
+    );
   } else return false;
 };
