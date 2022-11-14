14a57be3696e038b905c672f1c50ea8798c4b003
Jason Barr <jason.barr@metronetinc.com>
2022-11-14 08:25:48 -0600

    Nope, that was wrong

--- src/typechecker/bind.ts
+++ src/typechecker/bind.ts
@@ -111,7 +111,7 @@ export const bind = (node: ASTNode, env: TypeEnv, ty?: Type): BoundASTNode => {
       );
     case SyntaxNodes.LambdaExpression:
       // gets extended lambdaEnvironment from type checker
-      check(node, env, ty!);
+      check(node, ty!, env);
       const lambdaBody = bind((node as LambdaExpression).body, env);
       return BoundLambdaExpression.new(
         node as LambdaExpression,
