4f9881615345014e1bb66257ac5c3102338228e1
Jason Barr <jason.barr@metronetinc.com>
2022-11-14 08:03:05 -0600

    Parse variable declaration

--- src/syntax/parser/RuleParser.ts
+++ src/syntax/parser/RuleParser.ts
@@ -1,6 +1,9 @@
 import { LexResult } from "../lexer/LexResult";
+import { TokenNames } from "../lexer/TokenNames";
 import { TokenTypes } from "../lexer/TokenTypes";
+import { AssignmentExpression } from "./ast/AssignmentExpression";
 import { ASTNode } from "./ast/ASTNode";
+import { VariableDeclaration } from "./ast/VariableDeclaration";
 import { ExpressionParser } from "./ExpressionParser";
 
 export class RuleParser extends ExpressionParser {
@@ -12,6 +15,10 @@ export class RuleParser extends ExpressionParser {
     const token = this.reader.peek();
 
     switch (token.name) {
+      case TokenNames.Var:
+        return this.parseVariableDeclaration(false);
+      case TokenNames.Const:
+        return this.parseVariableDeclaration(true);
       default:
         throw new Error(`Parse rule not found for token name ${token.name}`);
     }
@@ -25,5 +32,21 @@ export class RuleParser extends ExpressionParser {
     }
 
     return this.parseExpression();
+  }
+
+  private parseVariableDeclaration(constant: boolean) {
+    const token = this.reader.next();
+    const start = token.location;
+    const assignment = this.parseExpression();
+
+    if (!(assignment instanceof AssignmentExpression)) {
+      throw new Error(
+        `Variable declaration must have an assignment as its expression`
+      );
+    }
+
+    const end = assignment.end;
+
+    return VariableDeclaration.new(assignment, constant, start, end);
   }
 }
